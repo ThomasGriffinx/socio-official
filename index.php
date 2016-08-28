2b8bfc6464687330d7985fbe19ded7f1f50ca42d
thomasgriffinx <xytomg@gmail.com>
2016-08-28 21:15:32 +0100

    update

--- index.php
+++ index.php
@@ -12,27 +12,20 @@
 </head>
 <body>
 
+
+
+
+
   <div class="parallax-container valign-wrapper">
     <div class="section no-pad-bot">
       <div class="container">
         <div class="row center">
+           <div class="row center">
           <h1 class="header col s12 light">'SAVE ME' New E.P</h1>
         </div>
           <div class="row center">
-          <h2 class="header col s12 light">SOCIO</h2>
+          <h2 class="header col s12 light">by SOCIO</h2>
         </div>
-      </div>
-    </div>
-    <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
-  </div>
-
-
-
-
-  <div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <div class="row center">
           <h5 class="header col s12 light">Coming Early September 2016</h5>
         </div>
       </div>
