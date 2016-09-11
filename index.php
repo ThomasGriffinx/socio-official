846250d4494d6b9a40da34b38c1fb00f3b9827de
thomasgriffinx <xytomg@gmail.com>
2016-09-11 18:39:25 -0400

    master update

--- index.php
+++ index.php
@@ -52,6 +52,19 @@
 </div>
 
 
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <h1 class="header center white-text"></h1>
+        <div class="row center">
+          <h5 class="header col s12 light"></h5>
+        </div>
+        <br><br>
+      </div>
+    </div>
+    <div class="parallax"><img src="SOCIO-SAVE ME ARTWORK.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
 
 
   <!--  Scripts-->
