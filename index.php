6608b1a27a3b60eacbd1701b3e063f18bc56d001
thomasgriffinx <xytomg@gmail.com>
2016-09-11 19:04:37 -0400

    px

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 .responsive-image {
   height: auto;
   width: 100%;
-	margin-left: 100px;
+	margin-left: 200px;
 }
 </style>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
