fb0962da41ec4ec4798f116e2546745a8daa7608
thomasgriffinx <xytomg@gmail.com>
2016-10-09 14:20:13 +0100

    commit

--- index.php
+++ index.php
@@ -38,37 +38,61 @@
   </div>
 
   <br> </br>
-<!--
+<!--Work-->
+<div class="section scrollspy" id="work">
+    <div class="container">
+        <h2 class="header text_b">Available On</h2>
         <div class="row">
-        <div class="col s12 m4">
-          <div class="icon-block">
-             <img src="amazonmusic.png" height="25%" width="25%">
-          </div>
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/project1.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">iTunes<i class="mdi-navigation-more-vert right"></i></span>
+                        <p><a href="https://itunes.apple.com/album/id1154163561?ls=1&app=itunes"></a></p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">Project Title <i class="mdi-navigation-close right"></i></span>
+                        <p>Here is some more information about this project that is only revealed once clicked on.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/project2.jpeg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">Project Title <i class="mdi-navigation-more-vert right"></i></span>
+                        <p><a href="#">Project link</a></p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">Project Title <i class="mdi-navigation-close right"></i></span>
+                        <p>Here is some more information about this project that is only revealed once clicked on.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/project3.png">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">Project Title <i class="mdi-navigation-more-vert right"></i></span>
+                        <p><a href="#">Project link</a></p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">Project Title <i class="mdi-navigation-close right"></i></span>
+                        <p>Here is some more information about this project that is only revealed once clicked on.</p>
+                    </div>
+                </div>
+            </div>
+           
+            </div>
         </div>
-
-        <div class="col s12 m4">
-          <div class="icon-block">
-           <img src="iTunes.png" height="25%" width="25%">
-          </div>
-        </div>
-
-        <div class="col s12 m4">
-          <div class="icon-block">
-             <img src="spotify.png" height="25%" width="25%">
-        </div>
-      </div>
-
     </div>
-  </div>
-
--->
-
-
-<!--
-<div class="responsive-image">
-<img src="banner.jpg" alt="Unsplashed background img 2">
- </div> 
--->
+</div>
 
 
   <!--  Scripts-->
